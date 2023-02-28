debc95dc72f6e6f6179022436f1c68e2a3478795
nchiorsac <natasha@okapps.net>
2023-03-01 01:21:57 +1100

    initial commit

---
+++ server/src/main/java/c3phal0p0d/project/octochess/OctochessApplication.java
@@ -0,0 +1,13 @@
+package c3phal0p0d.project.octochess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctochessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctochessApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/c3phal0p0d/project/octochess/OctochessApplicationTests.java
@@ -0,0 +1,13 @@
+package c3phal0p0d.project.octochess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctochessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
